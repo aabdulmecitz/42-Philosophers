3a0078dd70c03a5859db7ea6ca43d39a81af064f
Ahmet abdülmecit Özkaya <aozkaya@k1m30s07.42istanbul.com.tr>
2025-02-12 13:52:34 +0300

    Update: 2025-02-12 13:52:34

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aozkaya <aozkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 14:13:46 by aozkaya           #+#    #+#             */
-/*   Updated: 2025/02/12 11:59:07 by aozkaya          ###   ########.fr       */
+/*   Updated: 2025/02/12 13:52:25 by aozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void all_destroy(t_data *data)
 {
     int i;
+
     i = 0;
     while (i < data->num_philosophers)
     {
@@ -29,18 +30,14 @@ void all_destroy(t_data *data)
 int main(int argc, char *argv[])
 {
     t_data data;
-    
+
     if (argc != 5 && argc != 6)
         return 0;
     if (init_vars(argc, argv, &data) == 2)
         return (free(data.philosophers), 0);
-    
     if (create_philos(&data) == -1)
         return (1);
-        
-    usleep(100);
-    all_destroy(&data);
-    return (0);
+    return (usleep(100), all_destroy(&data),0);
 }
 
 // ./philo <number_of_philosophers> <time_to_die> <time_to_eat> <time_to_sleep> ?<number_of_times_each_philosopher_must_eat>
